ec76235f7f55bbf2a4e6905ff44df539ac637416
bllyanos <bllyanos@gmail.com>
2017-10-18 13:53:53 +0700

    sudah tambahin class Koneksi

--- src/master/Koneksi.java
+++ src/master/Koneksi.java
@@ -1,5 +1,23 @@
 package master;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Koneksi {
-    //kevin
+    private Connection con = null;
+
+    public Koneksi(){
+        getConnection();
+    }
+
+    public Connection getConnection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/cerdas_cermat_admin","root","");
+            return con;
+        } catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
