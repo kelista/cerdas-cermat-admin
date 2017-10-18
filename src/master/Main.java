7a77dc9879bf11d04acf55c0735bd65a9f3e96cd
bllyanos <bllyanos@gmail.com>
2017-10-18 13:17:01 +0700

    billy

--- src/master/Main.java
+++ src/master/Main.java
@@ -18,7 +18,8 @@ public class Main {
         System.out.println("hello world");
         System.out.println("Hello");
         System.out.println("apake");
-                
+
+        System.out.println("jelek");
     }
     
 }
