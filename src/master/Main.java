14dc89a7b372cd385b0b82b41f28521676366595
kelista <kevinchristiankelista@gmail.com>
2017-10-18 13:22:43 +0700

    asd
    wda

--- src/master/Main.java
+++ src/master/Main.java
@@ -16,9 +16,7 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("Hello");
-
-        System.out.println("jelek");
+        System.out.println("asdw");
         //check
     }
     
