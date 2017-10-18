9b9a47954fd3b247ec9f189b76c6788fdc43ce54
bllyanos <bllyanos@gmail.com>
2017-10-18 13:15:26 +0700

    billy

--- src/master/Main.java
+++ src/master/Main.java
@@ -16,7 +16,7 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("kevin love billy");
+        System.out.println("Hello");
     }
     
 }
